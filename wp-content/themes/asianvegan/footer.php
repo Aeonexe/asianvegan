c1bf2ce54f3a325823e0f4f232de40585e42cbb4
Albrt <albertohartzet@gmail.com>
2018-02-24 11:37:14 -0600

    form

--- wp-content/themes/asianvegan/footer.php
+++ wp-content/themes/asianvegan/footer.php
@@ -30,6 +30,7 @@
 
 								<?php endif; ?>
 
+								<?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2, 'title' => false, 'description' => false ) ); ?>
 							</div>
 
 						</div>
